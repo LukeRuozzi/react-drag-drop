6919b0f77fc5a1814ff6da5cf7bb4291f9cb8625
LukeRuozzi <luca.ruozzi@gmail.com>
2022-03-20 18:51:29 +0100

    2 files modified

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { useDropzone } from 'react-dropzone';
+import React, { useState, useEffect, useRef, createRef } from 'react';
+import Dropzone, { useDropzone } from 'react-dropzone';
 
 const img = {
   display: 'block',
   width: 'auto',
-  height: '100px',
+  height: '300px',
 };
 
 export const FileUpload = () => {
@@ -34,24 +34,6 @@ export const FileUpload = () => {
     return null;
   };
 
-  const props = {
-    accept: 'image/png,image/jpg,image/jpeg,application/pdf',
-    onDrop: onDropHandler,
-    maxFiles: 1,
-    validator: customValidator,
-  };
-
-  const { fileRejections, getRootProps, getInputProps } = useDropzone(props);
-
-  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-      {errors.map((e) => (
-        <div key={e.code}>{e.message}</div>
-      ))}
-    </li>
-  ));
-
   useEffect(() => {
     // Make sure to revoke the data uris to avoid memory leaks
     if (file) {
@@ -60,22 +42,49 @@ export const FileUpload = () => {
   }, [file]);
 
   return (
-    <section className="drag-drop">
-      <div {...getRootProps({ className: 'dropzone' })}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-      <aside>
-        <h4>Files</h4>
-        {file && (
-          <div>
-            {file.path} - {file.size}
-            <img src={file.preview} style={img} />
-          </div>
-        )}
-        <h4>Rejected files</h4>
-        <ul>{fileRejectionItems}</ul>
-      </aside>
-    </section>
+    <>
+      {!file && (
+        <>
+          <Dropzone
+            accept="image/png,image/jpg,image/jpeg,application/pdf"
+            onDrop={onDropHandler}
+            maxFiles={1}
+            validator={customValidator}
+          >
+            {({ getRootProps, getInputProps, fileRejections }) => (
+              <>
+                <div {...getRootProps({ className: 'dropzone' })}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                  {fileRejections.length > 0 && (
+                    <div>
+                      <h4>Rejected files</h4>
+                      <ul>
+                        {fileRejections.map(({ file, errors }) => (
+                          <>
+                            {errors.map((e) => (
+                              <span key={e.code}>{e.message}</span>
+                            ))}
+                          </>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+          </Dropzone>
+        </>
+      )}
+      {file && (
+        <aside>
+          {file && (
+            <div>
+              <img src={file.preview} style={img} />
+            </div>
+          )}
+        </aside>
+      )}
+    </>
   );
 };
